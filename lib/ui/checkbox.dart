0f8811088b9429f5a10eb89bdd93e0f25d9eba41
SuNandarMyint <sunandarpissaya19@gmail.com>
2020-01-23 11:10:23 +0630

    added component

---
+++ lib/ui/checkbox.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyCheckBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CheckBoxes'),
+      ),
+    );
+  }
+}
